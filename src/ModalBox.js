0f24a8f879e44da17953593c9453f51dee7c6e4b
David Mulholland <davidmulholland@Davids-iMac.local>
2020-11-20 12:20:33 +0000

    spinner and modal window added

--- src/ModalBox.js
+++ src/ModalBox.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { showModal, showSpinner } from "./reducers/shopReducer";
-import { useSelector, useDispatch } from "react-redux";
+import { showModal } from "./reducers/shopReducer";
+import { useDispatch } from "react-redux";
 
 function ModalBox(props) {
-  let showSpinner = useSelector((state) => state.shop.showSpinner);
   const dispatch = useDispatch();
 
   const closeModal = () => {
@@ -14,18 +13,18 @@ function ModalBox(props) {
   return (
     <div className="modal">
       <div className="container">
-        <h1>How to use and navigate this site</h1>
-        <h2>
+        <h4>How to use and navigate this site</h4>
+        <p>
           This e-commerce site is built with the latest version of Redux and
           uses React useSelector and useDispatch hooks to update the state in
           Redux. It connects to a backend API created with Express and MongoDB.
           The admin login section allows you to create products and add images
           which are dealt with on the server using the multer npm package.{" "}
           <hr></hr>
-          <h2>Login info</h2>
+          <p>Login info</p>
           <hr></hr> Admin login: username: admin@company.com, password: admin123
           <hr></hr>Customer login: username: user@shop.com, password: buy1
-        </h2>
+        </p>
 
         <Button variant="primary" onClick={() => closeModal()}>
           OK
