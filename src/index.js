362a510ebcb1aefd2dae865c7decb9b1f968a9dd
David Mulholland <davidmulholland@Davids-iMac.local>
2020-12-22 16:39:24 +0000

    more styling added to the site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import store from "./app/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import 'bootswatch/dist/solar/bootstrap.min.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,5 +12,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
